1d71111e350fc1f5365f6cd9382207ae29f1d11f
insomniabuddy <106839588+insomniabuddy@users.noreply.github.com>
2024-01-12 09:54:24 -0500

    Add files via upload

--- js/countries.js
+++ js/countries.js
@@ -1,17 +1,19 @@
 const countriesData = [
 
-{"countryId":11,"countryName":"Australia"}
-, {"countryId":12,"countryName":"Canada"}
-, {"countryId":4,"countryName":"China"}
-, {"countryId":10,"countryName":"Hong Kong"}
-, {"countryId":5,"countryName":"Japan"}
-, {"countryId":6,"countryName":"New Zealand"}
-, {"countryId":9,"countryName":"Phillipines"}
-, {"countryId":7,"countryName":"Souht Korea"}
-, {"countryId":2,"countryName":"South Korea"}
-, {"countryId":3,"countryName":"Taiwan"}
-, {"countryId":1,"countryName":"Thailand"}
-, {"countryId":8,"countryName":"USA"}
-
-];
-
+    {"countryId":12,"countryName":"Australia"}
+    , {"countryId":13,"countryName":"Canada"}
+    , {"countryId":4,"countryName":"China"}
+    , {"countryId":11,"countryName":"Hong Kong"}
+    , {"countryId":8,"countryName":"Italy"}
+    , {"countryId":5,"countryName":"Japan"}
+    , {"countryId":6,"countryName":"New Zealand"}
+    , {"countryId":10,"countryName":"Phillipines"}
+    , {"countryId":7,"countryName":"Souht Korea"}
+    , {"countryId":2,"countryName":"South Korea"}
+    , {"countryId":3,"countryName":"Taiwan"}
+    , {"countryId":1,"countryName":"Thailand"}
+    , {"countryId":9,"countryName":"USA"}
+    
+    ];
+    
+    
